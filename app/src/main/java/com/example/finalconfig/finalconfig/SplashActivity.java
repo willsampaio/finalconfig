369851c6dd119ec8ff84af78f701177e364c4291
willsampaio <will.sampaio13@outlook.com>
2017-10-06 20:01:12 -0300

    Adicionada SplashScreen

---
+++ app/src/main/java/com/example/finalconfig/finalconfig/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.finalconfig.finalconfig;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        esperarAbrirMain(1500);
+    }
+
+    private void esperarAbrirMain(int ms){
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, ms);
+    }
+}
